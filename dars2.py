535f9eb77aba4a2993e380b9de8c11770a58381c
shokh.515 <shokhjahon515@gmail.com>
2025-07-20 16:31:36 +0500

    dars2

---
+++ dars2.py
@@ -0,0 +1,7 @@
+#print("Assalom alaykum")
+##print("Xayrli tong!")
+#print('Men "Dell" markasidagi noutbuk sotib oldim')
+#print("""Odami ersang, demagil odami,
+#Oniki, yo'q xalq g'amidin g'ami""")
+# print('Odami ersang, demagil odami, \nOniki, yo\'q xalq g\'amidin g\'ami')
+#print("Odami ersang, demagil odami,\nOniki, yo'q xalq g'amidin g'ami")
