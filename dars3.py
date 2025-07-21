ff18bca73526bdf9828f34b3470b4f7b264ab541
shokh.515 <shokhjahon515@gmail.com>
2025-07-21 11:30:51 +0500

    mohirdev_darslari

---
+++ dars3.py
@@ -0,0 +1,29 @@
+#03-dars. PRINT() funksiyasi
+# Sana : 20.07.2025
+# Muallif: Shoxjahon Tuymurodov
+
+#ism = "Shoxjahon"
+#yosh = 20
+#print(ism)
+#print(yosh)
+
+#ism = "Alisher"
+#print(ism)
+
+#ism_familya = "Shoxjahon Tuymurodov"
+#print(ism_familya)
+
+matn = "Hello World!"
+print(matn)
+
+xabar = "Assalom alaykum"
+print(xabar)
+
+#class = 5
+#print(class)
+
+
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
