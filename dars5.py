8ca131046952159abfdbd740b66e5dc4971128f3
shokh.515 <shokhjahon515@gmail.com>
2025-08-07 19:53:32 +0500

    mohirdev_darslari

---
+++ dars5.py
@@ -0,0 +1,40 @@
+#0-dars. Sonlar 
+# Sana : 07.08.2025
+# Muallif: Shoxjahon Tuymurodov
+
+# a = 20
+# b = 5.5
+# temp = 36.6
+# aholi_soni = 7_594_376_567
+# print(type(a))
+# print(type(aholi_soni))
+# print(aholi_soni)
+# print("Aholi soni:", aholi_soni)
+
+# x, y, z = 10, 20.5, -30
+# print(x, y, z)
+
+# c = a * b
+# print("c ning qiymati:", c)
+
+# d = 100/2
+# print(d)
+# e = 100//2
+# print(e)
+
+# radius = 20
+# PI = 3.14
+# diametr = 2*radius
+# print("Aylananing diametri:", PI * diametr)
+
+# ism = "Davron"
+# yosh = 20
+# #yosh = str(yosh)
+# xabar = ism + " " + str(yosh) + " yoshda"
+# print(xabar) 
+
+t_yil = int(input("Tug'ilgan yilingizni kiriting: "))
+yosh = 2025 - t_yil
+print("Siz", yosh, "yoshda ekansiz.")
+
+temp = 36.6
