f3b84279bba6986644ee0181336f6cda85ddc54e
shokh.515 <shokhjahon515@gmail.com>
2025-08-07 17:48:32 +0500

    mohirdev_darslari

---
+++ dars4.py
@@ -0,0 +1,68 @@
+#04-dars. String 
+# Sana : 21.07.2025
+# Muallif: Shoxjahon Tuymurodov
+
+# shahar = "Toshkent"
+# viloyat = "Buxoro viloyati"
+# print(type(shahar))
+
+# kocha = "farhod ko'chasi"
+# print(type(kocha))
+# mahalla = "Sog'bon"
+# tuman = "Bodomzor"
+
+# ism = "Shoxjahon"
+# familiya = "Tuymurodov"
+# print(ism + familiya)
+
+# ism = "Shoxjahon"
+# familiya = "Tuymurodov"
+# print(ism + " " + familiya)
+
+# f string bilan
+
+# ism = "Shoxjahon"
+# familiya = "Tuymurodov"
+# ism_familya = (f"{ism} {familiya}")
+# print(ism_familya)
+
+# ism = "Shoxjahon"
+# familiya = "Tuymurodov"
+# print(f" Mening ismim {ism} familiyam {familiya}.")
+
+# print("Hello world!")
+# print("Hello \tWorld!")
+# print("Hello \nWorld!")
+
+# string metodlari
+
+# ism = "Shoxjahon"
+# familiya = "Tuymurodov"
+# ism_sharf = f"{ism} {familiya}"
+# print(ism_sharf)
+# print(ism_sharf.upper())
+
+# ism_sharf = ism_sharf.upper()
+# print(ism_sharf)
+
+# print(ism_sharf.lower())
+# print(ism_sharf.title())
+# print(ism_sharf.capitalize())
+# print(ism_sharf.swapcase())
+# print(ism_sharf.replace("Shoxjahon", "Shoxjahonbek"))
+
+# meva = "    Olma   "
+# print(meva)
+# print("Men" + meva.strip() + "ni yaxshi ko'raman")
+# print("Men" + meva.rstrip() + "ni yaxshi ko'raman")
+# print("Men" + meva.lstrip() + "ni yaxshi ko'raman")
+
+# input funksiyasi
+
+# ism = input("Ismingiz nima?")
+# print("Assalomu aleykum,", ism)
+# print("Assalomu aleykum, " + ism)
+# print("Assalomu aleykum, " +ism)
+
+# ism = input("Ismingiz nima?\n>>>")
+# print("Assalomu aleykum, " + ism.title())
