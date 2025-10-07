7a458dcd570f1e0ed078b454d947172ce9c05413
Shoxjahon <shokhjahon515@gmail.com>
2025-10-07 17:34:36 +0500

    mohirdev_darslari

---
+++ dars9.py
@@ -0,0 +1,55 @@
+# 09-dars. Tarmoqlanish (if else shartlari)  
+# Sana : 08.08.2025
+# Muallif: Shoxjahon Tuymurodov
+
+# avtomabillar = ['audi', 'bmw', 'mersades', 'tayotta', 'chevralet']
+
+# for avto in avtomabillar:
+#     if avto == 'bmw':
+#         print(avto.upper())
+#     else:
+#         print(avto.title())
+
+# ism = 'Shoxjahon'
+# ism.lower() == 'shokh'
+
+# ism = input('Ismingizni nima?\n>>>')
+# if ism.lower() != 'shoxjahon':
+#     print(f'Uzr, {ism.title()} biz Shoxjahonni kutyapmiz')
+# else:
+#     print('Shokh nima gap')
+
+
+# javob = float(input('12*6 nechchiga teng?>>>'))
+# if javob != 72:
+#     print("Javob xato")
+
+# yosh = int(input('Yoshingiz nechchida?>>>'))
+# if yosh >= 18:
+#     print('Hush kelibsiz dostim')
+# else:
+#     print('Siz hali yosh bolasiz Hoshimjon')
+
+# login = input('Yangi login tanlang:')
+# if len(login) >= 5:
+#     print('Loginingiz togri keldi')
+# else:
+#     print('Boshqa login tanlang')
+
+# yil = int(input("Tug'ulgan yiligizni kiriting:"))
+# if 2025-yil<18:
+#     print(f"Yoshingiz {2025-yil} da ekan")
+#     print("Kirish mumkin emas")
+# else: 
+#     print(f"Yoshingiz {2025-yil} dan katta ekan")
+#     print("Xush kelibsiz")
+
+# yosh = int(input("Yoshingiz nechchida?"))
+# if yosh >=60:
+#     print("Siz Covid 19 guruhidansiz")
+
+# x, y = 17, 20  
+# if x>y:
+#     print('x>y')
+# else:
+#      print('x<y')
